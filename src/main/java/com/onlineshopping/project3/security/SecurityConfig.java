38f73148e68f398db149c47172f4fcc470d68f6e
Gokdenizb <148850933+Gokdenizb@users.noreply.github.com>
2025-04-26 03:11:43 +0300

    ../auth/login formuna remember me butonu eklendi. Bu buton  işaretlendiğinde 1 saat süren bir cookie oluşturuluyor. SecurityConfigde securityFilterChaine rememberMe kısmı eklendi ve bu kısım için PersistentTokenRepository bean i oluşturuldu.

--- src/main/java/com/onlineshopping/project3/security/SecurityConfig.java
+++ src/main/java/com/onlineshopping/project3/security/SecurityConfig.java
@@ -1,21 +1,27 @@
 package com.onlineshopping.project3.security;
 
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, UserDetailsService userDetailsService, DataSource dataSource) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
 
@@ -33,6 +39,12 @@ public class SecurityConfig {
                         .defaultSuccessUrl("/", true)   // Login olursan direk ana sayfaya yolla
                         .permitAll()
                 )
+                .rememberMe(rm -> rm
+                        .rememberMeParameter("remember-me")
+                        .tokenRepository(persistentTokenRepository(dataSource))
+                        .userDetailsService(userDetailsService)
+                        .tokenValiditySeconds(3600)
+                )
                 .logout(logout -> logout
                         .logoutUrl("/logout")
                         .logoutSuccessUrl("/")
@@ -48,6 +60,18 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public PersistentTokenRepository persistentTokenRepository(DataSource dataSource){
+        JdbcTokenRepositoryImpl repository = new JdbcTokenRepositoryImpl();
+        //Eğer gerekli table dbde yoksa ilk çalıştırışta uncommentleyin.
+        //Fakat sonradan tekrar commentlemek gerekir yoksa hata alırsınız.
+        //repository.setCreateTableOnStartup(true);
+        repository.setDataSource(dataSource);
+        return repository;
+    }
+
+
 
 
 }
